f61d699ca669744e918c431fa58b9da0ad4ae443
Raeyan0786 <reyankhan01999@gmail.com>
2025-10-26 16:39:47 +0530

    first commit

--- src/components/DeleteDailog/index.tsx
+++ src/components/DeleteDailog/index.tsx
@@ -31,7 +31,7 @@ const DeleteDailog = ({handleDelete}:DeleteType) => {
                 Cancel
               </button>
               <button
-                onClick={()=>{handleDelete(); setIsDelete(false)}}
+                onClick={()=>{ setIsDelete(false); handleDelete();}}
                 className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
               >
                 Yes, delete it!
